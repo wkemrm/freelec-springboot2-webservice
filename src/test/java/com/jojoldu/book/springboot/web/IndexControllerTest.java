95e9b96ee6123cbdf489eaddf2e5a3cf837ae385
DESKTOP-DJD8IRH\7yhn8 <wkemrm12@naver.com>
2021-10-26 22:56:52 +0900

    메인페이지_로딩 테스트 코드 주석 처리 AWS 연결 때 문제가 일어남

--- src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
@@ -13,15 +13,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IndexControllerTest {
 
+
     @Autowired
     private TestRestTemplate restTemplate;
 
-/*    @Test
+
+    @Test
     public void 메인페이지_로딩() {
         //when
         String body = this.restTemplate.getForObject("/", String.class);
 
         //then
-        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
-    }*/
+        //assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+    }
 }
